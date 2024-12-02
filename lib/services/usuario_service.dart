f4d3379cba2cbb2f8efcbbcc7ee57ce3f40ffe31
Rafael Martinez <rafaelalfon@hotmail.com>
2024-12-02 13:27:00 -0600

    Cambio para tomar en otras ramas

--- lib/services/usuario_service.dart
+++ lib/services/usuario_service.dart
@@ -3,31 +3,7 @@ import 'dart:async';
 import 'package:estados_app/models/usuario.dart';
 
 class _UsuarioService {
-  Usuario? _usuario;
 
-  final StreamController<Usuario> _usuarioStreamController =
-      StreamController<Usuario>.broadcast();
-
-  Usuario? get usuario => _usuario;
-  bool get existeUsuario => (_usuario != null) ? true : false;
-
-  Stream<Usuario> get usuarioStream => _usuarioStreamController.stream;
-
-  void cargarUsuario(Usuario user) {
-    _usuario = user;
-    _usuarioStreamController.add(user);
-  }
-
-  void cambiarEdad(int edad) {
-    if (_usuario == null) return;
-
-    _usuario!.edad = edad;
-    _usuarioStreamController.add(_usuario!);
-  }
-
-  dispose() {
-    _usuarioStreamController.close();
-  }
 }
 
 final usuarioService = _UsuarioService();
